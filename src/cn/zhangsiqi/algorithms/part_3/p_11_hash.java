deac0adf8e37bcf6bf1e85e3de199d3a7b0ffe77
zhixuan <1905492410@qq.com>
2017-05-15 16:24:49 +0800

    tset

--- src/cn/zhangsiqi/algorithms/part_3/p_11_hash.java
+++ src/cn/zhangsiqi/algorithms/part_3/p_11_hash.java
@@ -1,7 +1,13 @@
 package cn.zhangsiqi.algorithms.part_3;
 
+import org.junit.Test;
+
 /**
  * Created by zhixuan on 2017/5/15.
  */
-public class p_11_hash {
+public class P_11_hash {
+    @Test
+    public void func1(){
+
+    }
 }
